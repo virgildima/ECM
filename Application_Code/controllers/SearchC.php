8bc1ae7c200bddb7866dc0304b7043960050fe3d
W.R.Boateng-Asare <wrb2@kent.ac.uk>
2017-01-03 15:31:08 +0000

    Update SearchC.php

--- Application_Code/controllers/SearchC.php
+++ Application_Code/controllers/SearchC.php
@@ -12,9 +12,11 @@ class SearchC extends CI_Controller {
 		$this->load->view('header');
 		$this->load->view('sidebar');
 		$this->load->view('search_form');
+	
 	}
 	public function execute_search()
 	{
+	    $this->load->model('search_model');
 		$search_term = $this->input->post('search');		
 		$data['results'] = $this->search_model->get_results($search_term);				
 		$this->load->view('search_results',$data);
