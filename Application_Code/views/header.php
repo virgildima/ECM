bbb9b864f2cd4aa1d3482d79540e8412d9018a47
Ross Taylor <rt345@kent.ac.uk>
2017-01-03 14:42:34 +0000

    Upload new file

---
+++ Application_Code/views/header.php
@@ -0,0 +1,56 @@
+<?php
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>Electronic Conference Management</title>
+<style type="text/css">
+
+body {
+	background-color: #fff;
+	margin: 40px;
+	font: 13px/20px normal Helvetica, Arial, sans-serif;
+	color: #4F5155;
+}
+
+a {
+	color: #003399;
+	background-color: transparent;
+	font-weight: normal;
+}
+
+h1 {
+	color: #444;
+	background-color: transparent;
+	border-bottom: 1px solid #D0D0D0;
+	font-size: 19px;
+	font-weight: normal;
+	margin: 0 0 14px 0;
+	padding: 14px 15px 10px 15px;
+}
+
+}
+</style>
+</head>
+<body>
+
+	<div id="container">
+		<div style='float: right;'>
+			<a href="../homepageC/home">Log out</a>
+		</div>
+		</br>
+		<div style='float: right;'>
+			<a href="../homepageC/home">Help</a>
+		</div>
+
+		<h1>ECM</h1>
+		<h5>Electronic Conference Management</h5>
+		</br>
+		
+		
+</div>
+</body>
+</html>
+	
