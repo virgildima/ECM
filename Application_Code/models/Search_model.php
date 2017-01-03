797c821475ad8f6f6f04375d3a37f1c767b2b5b1
Ross Taylor <rt345@kent.ac.uk>
2017-01-03 14:41:58 +0000

    Upload new file

---
+++ Application_Code/models/Search_model.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Search_model extends CI_Model {
+
+    public function get_results($search_term='default')
+    {
+    
+        $this->db->select('*');
+        $this->db->from('members');
+        $this->db->like('username',$search_term);
+
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+}
