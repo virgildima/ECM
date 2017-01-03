ac23ce787729345d8ec43119dffdd829fe7b7d99
Ross Taylor <rt345@kent.ac.uk>
2017-01-03 14:40:08 +0000

    Upload new file

---
+++ Application_Code/controllers/SearchC.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SearchC extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+	public function index()
+	{
+		$this->load->view('header');
+		$this->load->view('sidebar');
+		$this->load->view('search_form');
+	}
+	public function execute_search()
+	{
+		$search_term = $this->input->post('search');		
+		$data['results'] = $this->search_model->get_results($search_term);				
+		$this->load->view('search_results',$data);
+	}
+}
