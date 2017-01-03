25e025c4c0eedb8a968ca621e1388472974cb3ce
Ross Taylor <rt345@kent.ac.uk>
2017-01-03 14:43:39 +0000

    Upload new file

---
+++ Application_Code/views/upload_success.php
@@ -0,0 +1,26 @@
+<?php
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+?>
+<?php include_once ('header.php'); ?>
+<?php include_once ('sidebar.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Upload File </title>
+<style>
+#container3 {
+	position: fixed;
+	top: 120px;
+	left: 300px;
+	width: 320px;
+	height: 40px;
+	background-color: white;
+}
+</style>
+</head>
+<body>
+<div id="container3">
+<h3>Your file was successfully uploaded!</h3>
+</div>
+</body>
+</html>
