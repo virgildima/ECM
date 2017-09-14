1867fafb448085475907f749eaa1d36dad2e5a55
Virgil Dima <virgildima90@yahoo.com>
2017-09-14 01:14:22 +0100

    Add files via upload

---
+++ Application Code/ci_autocomplete/my_models.php
@@ -0,0 +1,13 @@
+<?php die();
+            /**
+            * Add you custom models here that you are loading in your controllers
+            * ---------------------- Models to Load ----------------------
+            *@property User $user
+@property UserHelper $userhelper
+*
+            */
+            class my_models
+            {
+            }
+
+            // End my_models.php
